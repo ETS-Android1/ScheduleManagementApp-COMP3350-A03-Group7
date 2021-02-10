3b9e99cb04e3879a12508cb2a5f39760cd9adf9f
Thai Tran <tranttt@myumanitoba.ca>
2021-02-09 18:18:08 -0600

    Our Skeleton App

---
+++ app/src/main/java/comp3350/team7/scheduleapp/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package comp3350.team7.scheduleapp.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.team7.scheduleapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
