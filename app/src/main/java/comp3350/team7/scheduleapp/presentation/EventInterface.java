0fc273689c38cefa20451b09872e66470857c9dd
T-ROY <royt34@myumanitoba.ca>
2021-02-18 17:02:50 -0600

    Created Event & EventInterface

---
+++ app/src/main/java/comp3350/team7/scheduleapp/presentation/EventInterface.java
@@ -0,0 +1,27 @@
+package comp3350.team7.scheduleapp.presentation;
+
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public interface EventInterface {
+
+    public int getID();
+    public String getTitle();
+    public String getDescription();
+    public String getLocation();
+    public LocalDateTime getStart();
+    public LocalDateTime getEnd();
+    public Duration getDuration();
+    public Event getNext();
+
+    public void setTitle(String title);
+    public void setDescription(String desc);
+    public void setLocation(String location);
+    public void setStart(int year, int month, int day, int hour, int minute);
+    public void setEnd(int year, int month, int day, int hour, int minute);
+    public void setNext(Event next);
+
+    public Event createEvent(String title, String desc, String location, Event next,
+                             int s_year, int s_month, int s_day, int s_hour, int s_minute,
+                             int e_year, int e_month, int e_day, int e_hour, int e_minute);
+}
