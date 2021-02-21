7b5fe3a444ddcdd4a98a0d5c35340931ee80e542
antonyanuraj <anuraja@myumanitoba.ca>
2021-02-21 15:00:50 -0600

    Added two new methods to User class
    TODO:
    
    checkIfUsernameExists =  just checks if the checkInUserData is true.
    checkInUserData = waiting to see which data structure we will be using for user data so that we can use the appropriate search method.
    
    If anymore suggestions, please let me know.

--- app/src/main/java/comp3350/team7/scheduleapp/presentation/User.java
+++ app/src/main/java/comp3350/team7/scheduleapp/presentation/User.java
@@ -70,4 +70,17 @@ public class User {
             throw new Exception("Your password needs to be 8-16 characters");
         }
     }
+
+    protected void checkIfUsernameExists(String user_id) {
+        if (checkInUserData(user_id)) {
+            System.out.println("Username already exists, please try another username!");
+        }
+    }
+
+    // this method would return true if username already exists in the database.
+    protected Boolean checkInUserData(String u) {
+        Boolean answer = false;
+        // going to be a search ability to go thought the data structure to find if the username exists in the database.
+        return answer;
+    }
 }
