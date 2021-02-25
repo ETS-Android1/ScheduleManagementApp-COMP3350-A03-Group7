02a2817e25222622db466aae5e5df3ae5fedcc31
Thai Tran <tranttt@myumanitoba.ca>
2021-02-24 20:36:27 -0600

    Update LogTag

---
+++ app/src/main/java/comp3350/team7/scheduleapp/logic/logTag/TAG.java
@@ -0,0 +1,22 @@
+package comp3350.team7.scheduleapp.logic.logTag;
+
+/*
+ * Created By Thai Tran on 24 February,2021
+ *
+ */
+
+
+public enum TAG {
+    ScrollingActivity("ScrollingActivity"),
+    RecyclerViewOnItemtouchHelper("RecylerViewItemTouchHelper"),
+    RecyclerViewAdapter("RecyclerViewAdapter");
+    String text;
+
+    TAG(String text) {
+        this.text = text;
+    }
+
+    public String toString() {
+        return text;
+    }
+}
