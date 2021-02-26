bdf695af1188d39e80597ae00ea5de4d4725edae
Thai Tran <tranttt@myumanitoba.ca>
2021-02-26 13:04:39 -0600

    update EventTest

--- app/src/test/java/comp3350/team7/scheduleapp/EventTest.java
+++ app/src/test/java/comp3350/team7/scheduleapp/EventTest.java
@@ -29,27 +29,12 @@ public class EventTest {
         testEvent.setTitle("New Title");
         assertEquals("Both title should 'New Title'", "New Title", testEvent.getTitle());
     }
-    @Test
-    public void testTooLongTitle() {
-        assertThrows("This should fail as title too long", ArithmeticException.class,
-                () -> testEvent.setTitle("This title is way too long you need to change it to something shorter"));
-    }
-    @Test
-    public void testTooShortTitle() {
-        assertThrows("This should fail as title too short", ArithmeticException.class,
-                () -> testEvent.setTitle(""));
-    }
+
     @Test
     public void testAcceptableDescription() {
         testEvent.setDescription("Event description");
         assertEquals("Event description should change to 'Event Description'",
                 "Event description", testEvent.getDescription());
     }
-    @Test
-    public void testTooLongDescription() {
-        assertThrows("This should fail as title too long", ArithmeticException.class,
-                () -> testEvent.setDescription("Description Description Description Description Description Description " +
-                        "Description Description Description Description Description Description Description Description " +
-                        "Description Description Description Description Description Description Description Description "));
-    }
+
 }
