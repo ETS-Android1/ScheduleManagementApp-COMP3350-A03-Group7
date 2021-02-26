b2674e2bf4e202270784c120bd18888d7e17dbb4
antonyanuraj <anuraja@myumanitoba.ca>
2021-02-26 15:33:18 -0600

    Added AllTests

---
+++ app/src/test/java/comp3350/team7/scheduleapp/AllTests.java
@@ -0,0 +1,37 @@
+package comp3350.team7.scheduleapp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * Created By Thai Tran on 26 February,2021
+ *
+ */
+
+/*
+ * Created By Thai Tran on 26 February,2021
+ *
+ */
+
+/*
+ * Created By Thai Tran on 26 February,2021
+ *
+ */
+
+/*
+ * Created By Thai Tran on 26 February,2021
+ *
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        EventControllerTest.class,
+        EventDbStubTest.class,
+        EventTest.class,
+        EventValidatorTest.class,
+        UserTest.class
+})
+
+public class AllTests {
+
+}
