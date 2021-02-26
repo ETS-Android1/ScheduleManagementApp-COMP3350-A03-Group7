18553b076b0dfe79196464b1f3856aaf0ba8db75
antonyanuraj <anuraja@myumanitoba.ca>
2021-02-26 15:36:49 -0600

    Added AllTests

--- app/src/test/java/comp3350/team7/scheduleapp/AllTests.java
+++ app/src/test/java/comp3350/team7/scheduleapp/AllTests.java
@@ -3,26 +3,6 @@ package comp3350.team7.scheduleapp;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/*
- * Created By Thai Tran on 26 February,2021
- *
- */
-
-/*
- * Created By Thai Tran on 26 February,2021
- *
- */
-
-/*
- * Created By Thai Tran on 26 February,2021
- *
- */
-
-/*
- * Created By Thai Tran on 26 February,2021
- *
- */
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         EventControllerTest.class,
