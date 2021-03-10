b4745c6790f4a78ea977991ef87d226537842a4c
Aaron Joson <josona@myumanitoba.ca>
2021-03-10 11:25:05 -0600

    Main that makes UserDB.script (if i did it right)

---
+++ app/src/main/java/comp3350/team7/scheduleapp/Application/Main.java
@@ -0,0 +1,32 @@
+package comp3350.team7.scheduleapp.Application;
+
+/*
+ * Created By Thai Tran on 10 March,2021
+ *
+ */
+
+public class Main {
+    private static String userDBName = "UserDB";
+
+    public static void Main(String[] args){
+        System.out.println("Booting up Schedule Management Application.");
+        //run app
+    }
+
+    public static void setDBPathName(final String pathName){
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        userDBName = pathName;
+    }
+
+    public static String getDBPathName(){
+        return userDBName;
+    }
+}
