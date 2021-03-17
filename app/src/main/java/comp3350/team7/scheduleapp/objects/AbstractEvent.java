035863dc5e25c123bee73185536d7558d3bd3c38
Thai Tran <tranttt@myumanitoba.ca>
2021-03-17 12:14:18 -0500

    Remove IEvent, Create AbstractEvent
    We need to use abstract class for Event, not inteface

---
+++ app/src/main/java/comp3350/team7/scheduleapp/objects/AbstractEvent.java
@@ -0,0 +1,48 @@
+package comp3350.team7.scheduleapp.objects;
+
+import java.util.Calendar;
+
+import comp3350.team7.scheduleapp.logic.exceptions.InvalidEventException;
+import comp3350.team7.scheduleapp.objects.Event;
+
+public abstract class AbstractEvent {
+
+    final int MAX_DESCRIPTION_LENGTH = 240;
+    final int MAX_TITLE_LENGTH = 30;
+
+    final int PRIORITY_HIGH = 0;
+    final int PRIORITY_MEDIUM = 1;
+    final int PRIORITY_LOW = 2;
+
+    public int getID() {
+        return 0;
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public String getDescription() {
+
+
+    }
+
+    public void setID(int eid) {
+    }
+
+    public void setTitle(String title) throws InvalidEventException {
+
+    }
+
+    public void setDescription(String desc) throws InvalidEventException {
+
+    }
+
+    public void setStart(Calendar calendar) {
+
+    }
+
+    public void setEnd(Calendar calendar) {
+
+    }
+}
