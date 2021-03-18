0fa4d4729e36e1227780435af7ae7b3e369e6cf9
Thai Tran <tranttt@myumanitoba.ca>
2021-03-18 13:29:36 -0500

    inject dependency, error handling

--- app/src/main/java/comp3350/team7/scheduleapp/logic/ScheduleController.java
+++ app/src/main/java/comp3350/team7/scheduleapp/logic/ScheduleController.java
@@ -1,29 +1,41 @@
 package comp3350.team7.scheduleapp.logic;
 
+import android.util.Log;
+
 import java.util.Calendar;
 import java.util.List;
 
 import comp3350.team7.scheduleapp.application.DbServiceProvider;
+import comp3350.team7.scheduleapp.logic.exceptions.DbErrorException;
+import comp3350.team7.scheduleapp.logic.exceptions.ScheduleControllerException;
 import comp3350.team7.scheduleapp.objects.Event;
 import comp3350.team7.scheduleapp.persistence.SchedulePersistenceInterface;
 
 public class ScheduleController {
-
+    private static final String TAG = "ScheduleController";
+    private String userName;
     private List<Event> scheduleEvents;
     private SchedulePersistenceInterface scheduleDB;
 
-    public ScheduleController() {
+
+    public ScheduleController(String username,Calendar date) {
+        this.userName = username;
+        this.scheduleDB = DbServiceProvider
+                .getInstance()
+                .getSchedulePersistence();
+
+    }
+    public ScheduleController(SchedulePersistenceInterface scheduleDB, String username) {
+        this.scheduleDB = scheduleDB;
+        this.userName = username;
         scheduleEvents = null;
-        scheduleDB = DbServiceProvider.getInstance().getSchedulePersistence();
     }
-    public ScheduleController(){
-
-    }
-
-    public List<Event> getScheduleForDay(String username, Calendar specificDate) {
-        scheduleEvents = scheduleDB.getScheduleForUserOnDate(username, specificDate);
-        if (scheduleEvents.size() == 0) {
-            System.out.println("You don't have any events on this day");
+    public List<Event> getScheduleForDay(Calendar specificDate) throws ScheduleControllerException {
+        try{
+            scheduleEvents = scheduleDB.getScheduleForUserOnDate(this.userName, specificDate);
+        }catch (DbErrorException e){
+            Log.e(TAG,e.getMessage());
+            throw new ScheduleControllerException(e.getMessage(),e);
         }
         return scheduleEvents;
     }
