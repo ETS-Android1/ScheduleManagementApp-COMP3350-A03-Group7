b477c5cfad5f5c184be583ee1393f71095932c9c
Thai Tran <tranttt@myumanitoba.ca>
2021-03-18 10:36:46 -0500

    Add mock test, mainly test behavior of the event sorting

---
+++ app/src/test/java/comp3350/team7/scheduleapp/Helper/TestHelper.java
@@ -0,0 +1,21 @@
+package comp3350.team7.scheduleapp.Helper;
+
+/*
+ * Created By Thai Tran on 18 March,2021
+ *
+ */
+
+import java.io.File;
+import java.io.IOException;
+import com.google.common.io.Files;
+import comp3350.team7.scheduleapp.Application.Main;
+public class TestHelper {
+    private static final File dbScript = new File("src/main/assets/db/schedule.script");
+
+    public static File cloneDb() throws IOException {
+        final File tempDbScript = File.createTempFile("tempDbScript", ".script");
+        Files.copy(dbScript, tempDbScript);
+        Main.setDBPathName(tempDbScript.getAbsolutePath().replace(".script", ""));
+        return tempDbScript;
+    }
+}
