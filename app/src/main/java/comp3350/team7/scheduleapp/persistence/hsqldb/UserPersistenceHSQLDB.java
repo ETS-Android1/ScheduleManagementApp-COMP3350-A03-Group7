8fdd7b93b64ac761a7be99907a95aa8b381b9961
T-ROY <royt34@myumanitoba.ca>
2021-03-19 12:43:09 -0500

    Fixed queries for adding into the DBs

--- app/src/main/java/comp3350/team7/scheduleapp/persistence/hsqldb/UserPersistenceHSQLDB.java
+++ app/src/main/java/comp3350/team7/scheduleapp/persistence/hsqldb/UserPersistenceHSQLDB.java
@@ -82,7 +82,7 @@ public class UserPersistenceHSQLDB implements UserPersistenceInterface {
     @Override
     public User addUser(User newUser) throws UserDBException {
         try(final Connection c = connection()) {
-            final PreparedStatement msg = c.prepareStatement("INSERT INTO USERS VALUES(?,?,?,?)");
+            final PreparedStatement msg = c.prepareStatement("INSERT INTO USERS(?,?,?,?)");
             msg.setString(1, newUser.getUserId());
             msg.setString(2, newUser.getPassword());
             msg.setString(3, newUser.getFirstName());
