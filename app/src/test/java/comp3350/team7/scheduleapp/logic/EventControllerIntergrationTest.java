7a71544bb17a3fab3d575f748b5e2d7a8ca19b94
antonyanuraj <anuraja@myumanitoba.ca>
2021-03-19 03:57:45 -0500

    Added some tests to EventControllerIntergrationTest
    - still need to finish two tests (testUpdateEvent and testSortingEvent)

--- app/src/test/java/comp3350/team7/scheduleapp/logic/EventControllerIntergrationTest.java
+++ app/src/test/java/comp3350/team7/scheduleapp/logic/EventControllerIntergrationTest.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
 
 import comp3350.team7.scheduleapp.Helper.TestHelper;
+import comp3350.team7.scheduleapp.logic.exceptions.EventControllerException;
+import comp3350.team7.scheduleapp.objects.Event;
 import comp3350.team7.scheduleapp.persistence.EventPersistenceInterface;
 import comp3350.team7.scheduleapp.persistence.hsqldb.EventPersistenceHSQLDB;
 
@@ -25,42 +29,72 @@ public class EventControllerIntergrationTest {
     private File tempDb;
     private EventController eventController;
     private EventPersistenceInterface eventPersistence;
+    private Calendar testStartDate, testEndDate;
     @Before
     public void setUp() throws IOException {
         this.tempDb = TestHelper.cloneDb();
         this.eventPersistence = new EventPersistenceHSQLDB(this.tempDb.getAbsolutePath().replace(".script", ""));
-
         eventController = new EventController(eventPersistence, "username");
+        testStartDate = Calendar.getInstance();
+        testStartDate.set(2021,8,9);
+        testEndDate = Calendar.getInstance();
+        testEndDate.set(2021,8,10);
     }
 
     @Test
-    public void testAddEvent(){
-
+    public void testAddEvent() throws EventControllerException {
+        System.out.println("Starting testAddEvent");
+        final Event eventForAddEvent = eventController.CreateEvent("username","testAddEvent", "this is a test event", testStartDate);
+        eventController.addEvent(eventForAddEvent);
+        System.out.println("Finished testAddEvent");
     }
 
     @Test
-    public void testGetEventList(){
-
+    public void testGetEventList() throws EventControllerException {
+        System.out.println("Starting testGetEventList");
+        final List<Event> testEventList;
+        testEventList = eventController.getEventList("username");
+        assertNotNull(testEventList);
+        System.out.println("Finished testGetEventList");
     }
 
     @Test
-    public void testCreateEvent(){
-
+    public void testCreateEvent() throws EventControllerException {
+        System.out.println("Starting testCreateEvent");
+        final Event createdEvent;
+        final Event createdEvent1;
+        createdEvent = eventController.CreateEvent("username","testCreateEvent", "this is a test event", testStartDate);
+        assertNotNull(createdEvent);
+        createdEvent1 = eventController.CreateEvent("username","secondCreateEvent", "this is another test event", testStartDate, testEndDate);
+        assertNotNull(createdEvent1);
+        System.out.println("Finished testCreateEvent");
     }
 
     @Test
-    public void testRemoveEvent(){
-
+    public void testRemoveEvent() throws EventControllerException {
+        System.out.println("Starting testRemoveEvent");
+        final Event createdEvent3;
+        createdEvent3 = eventController.CreateEvent("username","testRemoveEvent", "this is a test event", testStartDate);
+        eventController.addEvent(createdEvent3);
+        final List<Event> testEventList1 = eventController.getEventList("username");
+        final int sizeOfList = testEventList1.size();
+        eventController.removeEvent(createdEvent3);
+        assertEquals(sizeOfList-1,(eventController.getEventList("username")).size());
+        System.out.println("Finished testRemoveEvent");
     }
 
     @Test
     public void testUpdateEvent(){
+        System.out.println("Starting testUpdateEvent");
 
+        System.out.println("Finished testUpdateEvent");
     }
 
     @Test
     public void testSortingEvent(){
+        System.out.println("Starting testSortingEvent");
 
+        System.out.println("Finished testSortingEvent");
     }
 
     @After
