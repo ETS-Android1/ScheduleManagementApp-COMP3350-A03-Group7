6894a2eb7602e09383cfde4c63deda829a9d045d
Thai Tran <tranttt@myumanitoba.ca>
2021-03-31 00:32:11 -0500

    Update date/time display format and timezone

--- app/src/main/java/comp3350/team7/scheduleapp/logic/TimeController.java
+++ app/src/main/java/comp3350/team7/scheduleapp/logic/TimeController.java
@@ -5,16 +5,34 @@ package comp3350.team7.scheduleapp.logic;
  *
  */
 
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class TimeController {
+    private static final String TAG = "TimeController";
     private static final long OffSetInMills = 6000;
+    private static final String DATE_FORMAT = "dd-MM-yyyy";
+    private static final String TIME_FORMAT = "HH:mm";
+    private static final Locale location =  Locale.getDefault();
     public static long minToMilliseconds(int min){
         return min * OffSetInMills;
     }
     public static int millisecondsToMin(long millis){
         return (int) (millis/ OffSetInMills);
+    }
+    public static String dateFormatHelper(Calendar cal){
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT,location);
+        Log.d(TAG,cal.getTimeZone().getDisplayName());
+        return formatter.format(cal.getTime());
+    }
+    public static String timeFormatHelper(Calendar cal){
+        SimpleDateFormat formatter = new SimpleDateFormat(TIME_FORMAT,location);
+        Log.d(TAG,cal.getTimeZone().getDisplayName());
+        return formatter.format(cal.getTime());
     }
 
 }
