9107ea7a93ea1ca9f18006b03758128efa72d939
Thai Tran <tranttt@myumanitoba.ca>
2021-04-01 18:58:13 -0500

    Add Test Helper Funtion
    Signed-off-by: Thai Tran <tranttt@myumanitoba.ca>

--- app/src/androidTest/java/comp3350/team7/scheduleapp/ultils/TestHelper.java
+++ app/src/androidTest/java/comp3350/team7/scheduleapp/ultils/TestHelper.java
@@ -9,10 +9,9 @@ import android.app.Activity;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-import androidx.constraintlayout.helper.widget.Layer;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -20,45 +19,41 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.hamcrest.Matchers;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import comp3350.team7.scheduleapp.R;
 import comp3350.team7.scheduleapp.ToastMatcher;
-import comp3350.team7.scheduleapp.presentation.adapter.RecyclerViewAdapter;
+import comp3350.team7.scheduleapp.application.UserClient;
+import comp3350.team7.scheduleapp.logic.EventController;
+import comp3350.team7.scheduleapp.logic.exceptions.EventControllerException;
+import comp3350.team7.scheduleapp.objects.Event;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.startsWith;
 
 
 public class TestHelper {
     private static final String PACKAGE_NAME = "comp3350.team7.scheduleapp";
-
-    public static void intentSetup() {
-        Intents.init();
-    }
-
-    public static void intentTeardown() {
-        Intents.release();
-    }
 
     public static void loginSetup() {
         onView(ViewMatchers.withId(R.id.LoginUsernameInput))
@@ -76,9 +71,7 @@ public class TestHelper {
     }
     public static Calendar getCustomizeCalendarInstance(int field,int offset){
         Calendar cal = Calendar.getInstance();
-//        cal.roll(Calendar.YEAR, true);
         cal.roll(Calendar.MONTH, true);
-//        cal.roll(field, true);
         cal.add(field,offset);
         return cal;
 
@@ -99,7 +92,7 @@ public class TestHelper {
 
     public static void setTime(int datePickerLaunchViewId, Calendar cal) {
         int hour, min;
-        hour = cal.get(Calendar.HOUR);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
         min = cal.get(Calendar.MINUTE);
         onView(withId(datePickerLaunchViewId)).perform(click());
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(hour, min));
@@ -122,15 +115,54 @@ public class TestHelper {
         return activityRef.get();
     }
 
-    public static void testMatchReceivedIntent(String activity){
-        Intents.intended(allOf(hasComponent(activity),toPackage(PACKAGE_NAME)));
+    public static void testMatchReceivedIntent(String activity) {
+        Intents.intended(allOf(hasComponent(activity), toPackage(PACKAGE_NAME)));
     }
-    public static <T> void testMatchReceivedIntentData(String key, T value){
-       Intents.intended(hasExtra(key, value));
+
+    public static <T> void testMatchReceivedIntentData(String key, T value) {
+        Intents.intended(hasExtra(key, value));
     }
-    public static DataInteraction onLayerAt(int listPosition) {
-        return onData(instanceOf(RecyclerViewAdapter.MyViewHolder.class))
-                .inAdapterView(withId(R.id.recylerview))
-                .atPosition(listPosition);
+
+    public static void matchItemAtPositionOnRecyclerView(int Position, String text) {
+        onView(allOf(withId(R.id.recylerview), isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(Position, click()))
+                .check(matches(hasDescendant(withText(containsString(text)))));
+    }
+
+    public static void matchItemWithTextOnRecyclerView(String text) {
+        ViewInteraction viewInteraction = onView(allOf(withId(R.id.recylerview),
+                hasDescendant(withText(containsString(text))),
+                isDisplayed()));
+        viewInteraction.perform(RecyclerViewActions.scrollTo(hasDescendant(withText(containsString(text)))));
+        viewInteraction.perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(containsString(text))),
+                click()));
+    }
+
+    public static int matchEventListSize(EventController eContrl, int size) {
+        int eventsSize = -1;
+        try {
+            eventsSize = eContrl.getEventListLength(UserClient.getUserId());
+            assertThat(eventsSize, Matchers.equalTo(size));
+        } catch (EventControllerException e) {
+            e.printStackTrace();
+        }
+        return eventsSize;
+    }
+
+    public static void cleanDb(EventController eCtrl) {
+        try {
+            List<Event> list = eCtrl.getEventList(UserClient.getUserId());
+            for (int i = 0; i < list.size(); i++) {
+                eCtrl.removeEvent(list.get(i));
+            }
+        } catch (EventControllerException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void matchDisplayDialogMessage(String dialogMessage) {
+        onView(withText(containsString(dialogMessage)))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
     }
 }
