f721c31a03788d2b9f6716a7c44b5629b99fce1b
Thai Tran <tranttt@myumanitoba.ca>
2021-04-05 00:27:25 -0500

    Rename RemoveEventSystemTest -> RemoveUndoEventSystemTest

---
+++ app/src/androidTest/java/comp3350/team7/scheduleapp/AlarmServiceSystemTest.java
@@ -0,0 +1,9 @@
+package comp3350.team7.scheduleapp;
+
+/*
+ * Created By Thai Tran on 04 April,2021
+ *
+ */
+
+public class AlarmServiceSystemTest {
+}
--- app/src/androidTest/java/comp3350/team7/scheduleapp/RemoveEventSystemTest.java
+++
@@ -1,9 +0,0 @@
-package comp3350.team7.scheduleapp;
-
-/*
- * Created By Thai Tran on 28 March,2021
- *
- */
-
-public class RemoveEventSystemTest {
-}
