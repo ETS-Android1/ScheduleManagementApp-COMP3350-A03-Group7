897f5ace5ca27f9359b0c0949abc636b82d09ef7
Thai Tran <tranttt@myumanitoba.ca>
2021-04-06 11:19:48 -0500

    Add helper boolean isSorted(), call it when we want to test whether the some list is sorted or not. We can also add a comparator class to specify in which order the list is sorted.
    Signed-off-by: Thai Tran <tranttt@myumanitoba.ca>

--- app/src/test/java/comp3350/team7/scheduleapp/Helper/TestHelper.java
+++ app/src/test/java/comp3350/team7/scheduleapp/Helper/TestHelper.java
@@ -9,6 +9,7 @@ import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Comparator;
 
 import comp3350.team7.scheduleapp.application.DbClient;
 
@@ -20,5 +21,13 @@ public class TestHelper {
         Files.copy(dbScript, tempDbScript);
         DbClient.setDBPathName(tempDbScript.getAbsolutePath().replace(".script", ""));
         return tempDbScript;
+    }
+
+    public static boolean isSorted(Object[] array, Comparator<Object> comparator, int length) {
+        if (array == null || length < 2)
+            return true;
+        if (comparator.compare(array[length - 2], array[length - 1]) > 0)
+            return false;
+        return isSorted(array, comparator, length - 1);
     }
 }
