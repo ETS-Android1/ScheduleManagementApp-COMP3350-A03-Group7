5b4d344f2399a9be54f6a5c31b941e761c72b021
Aaron Joson <josona@myumanitoba.ca>
2021-04-12 10:44:39 -0500

    Upload New File

---
+++ app/src/androidTest/java/comp3350/team7/scheduleapp/AllAcceptanceTests.java
@@ -0,0 +1,15 @@
+package comp3350.team7.scheduleapp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * Created By Thai Tran on 12 April,2021
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoginAcceptanceTest.class
+})
+public class AllAcceptanceTests {
+}
