942a935aaabc20eae4e90deaa15ab610f640e083
Aaron Joson <josona@myumanitoba.ca>
2021-04-15 17:57:21 -0500

    REFACTOR CODE Fixed missing comma and import

--- app/src/test/java/comp3350/team7/scheduleapp/AllIntegrationTests.java
+++ app/src/test/java/comp3350/team7/scheduleapp/AllIntegrationTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp3350.team7.scheduleapp.logic.EventControllerIntergrationTest;
+import comp3350.team7.scheduleapp.logic.IntegrationTest.UserDBManagerIT;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        EventControllerIntergrationTest.class
+        EventControllerIntergrationTest.class,
         UserDBManagerIT.class
 })
 
